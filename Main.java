3d177eb5071fb49459b5c2d3ffcc5d3b39eaa7f5
Mamidala-PhaniRupa <31915417+Mamidala-PhaniRupa@users.noreply.github.com>
2017-09-13 12:39:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student s1= new Student(1234,'Phani Mamidala','1997-06-04',656.4);
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
